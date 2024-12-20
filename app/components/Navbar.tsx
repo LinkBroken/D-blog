32f3889a6bb129ab6e36404809af066dd13db237
omer mohamed <damouramory@gmail.com>
2024-12-20 18:22:43 +0200

    chore: navbar styling fixes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,24 @@
 import Link from "next/link";
 
-export default function Navbar(){
-    return(
-        <div className="flex justify-evenly text-white bg-teal-700 p-2">
-        <h1 className="self-center text-2xl text-white ">D-blog</h1>
-        <Link className="hover:bg-teal-800 p-2 rounded-xl" href="/">Home</Link>
-        <Link className="hover:bg-teal-800 p-2 rounded-xl " href="/posts">Posts</Link>
-        <Link className="hover:bg-teal-800 p-2 rounded-xl " href="/users">Users</Link>
-        <Link className="bg-teal-700 hover:bg-teal-800  p-2 rounded-xl " href="/login">Login</Link>
-
-        </div>
-    )
+export default function Navbar() {
+  return (
+    <nav className="flex justify-evenly text-white bg-teal-700 p-4">
+      <h1 className="self-center text-2xl text-white ">D-blog</h1>
+      <Link className="hover:bg-teal-800 p-2 rounded-xl" href="/">
+        Home
+      </Link>
+      <Link className="hover:bg-teal-800 p-2 rounded-xl " href="/posts">
+        Posts
+      </Link>
+      <Link className="hover:bg-teal-800 p-2 rounded-xl " href="/users">
+        Users
+      </Link>
+      <Link
+        className="bg-teal-700 hover:bg-teal-800  p-2 rounded-xl "
+        href="/login"
+      >
+        Login
+      </Link>
+    </nav>
+  );
 }

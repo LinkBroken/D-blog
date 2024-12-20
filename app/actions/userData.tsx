2283dbfe368583525345dd03c9bc20b9fbbb5660
omer mohamed <damouramory@gmail.com>
2024-12-20 18:21:51 +0200

    chore: reformat code

--- app/actions/userData.tsx
+++ app/actions/userData.tsx
@@ -1,24 +1,15 @@
-"use server"
-// import { revalidatePath } from "next/cache";
-// import postData from "./postData";
-import prisma from "../api/_base"
+"use server";
 
-export default async function userData(route:string){
-    const data = prisma.user.findMany(
-        {
-            where: {
-                id: parseInt(route)
-            },
-            include:{
-                posts:true
-        }}
-    )
-    return await data
+import prisma from "../api/_base";
+
+export default async function userData(route: string) {
+  const data = prisma.user.findMany({
+    where: {
+      id: parseInt(route),
+    },
+    include: {
+      posts: true,
+    },
+  });
+  return await data;
 }
-
-
-// Checkout for the whole cart, for the total price
-// Ability to adjust quantity on the cart page
-// Reciept for the cart page
-
-

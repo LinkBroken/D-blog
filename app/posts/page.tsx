b8e992659050c2e70594d3947e5b09e665f3fc22
omer mohamed <damouramory@gmail.com>
2024-12-20 16:20:39 +0200

    feat: turn posts page fully server

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,21 +1,37 @@
-import { PrismaClient } from "@prisma/client";
-import Userdata from "../components/postData";
-import Image from "next/image";
 import { revalidatePath } from "next/cache";
-export default async function posts() {
-  const prisma = new PrismaClient()
-  const usersInfo = await prisma.post.findMany()
-  revalidatePath("/posts")
+import Image from "next/image";
+import Link from "next/link";
 
-  
+import prisma from "@/app/api/_base";
+import PostData from "../components/PostData";
+import { Button } from "@/components/ui/button";
+
+export default async function posts() {
+  const usersInfo = await prisma.post.findMany();
+  revalidatePath("/posts");
+
   return (
-    <div className=" bg-teals-50 overflow-hidden flex justify-around flex-col  items-center pt-8 ">
-    <div className=" relative right-1/4  overflow-hidden flex justify-evenly items-center gap-3 ">
-      <Image className="rounded-2xl" width={500} height={600} src="https://www.pixibitdesign.com/wp-content/uploads/2018/08/Blog-images-01.png" alt="alt text"></Image>
-    </div>
-    <div className="flex flex-col w-screen items-center">
-      {usersInfo? <Userdata usersInfo={usersInfo} />: <h1>No posts yet</h1>}
-    </div>
-    </div>
+    <section className=" bg-teals-50 overflow-hidden flex justify-around flex-col  items-center pt-8 ">
+      <div className=" relative overflow-hidden flex justify-evenly items-center gap-3 xl:right-1/4  ">
+        <Image
+          className=" w-4/5 rounded-2xl xl:w-full"
+          width={500}
+          height={600}
+          src="https://www.pixibitdesign.com/wp-content/uploads/2018/08/Blog-images-01.png"
+          alt="alt text"
+        ></Image>
+      </div>
+      <div className="flex flex-col w-screen items-center">
+        {usersInfo ? <PostData usersInfo={usersInfo} /> : <h1>No posts yet</h1>}
+      </div>
+      <div className="flex flex-col items-center">
+        <Button
+          className="w-1/8 mb-10 shadow-md shadow-black"
+          variant={"secondary"}
+        >
+          <Link href="/add">Add a post</Link>
+        </Button>
+      </div>
+    </section>
   );
 }

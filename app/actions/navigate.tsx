577a8288a5bbae6f83737dfd52b33d5811fbec37
omer mohamed <damouramory@gmail.com>
2024-12-20 18:20:39 +0200

    feat: add types for server action arguments

--- app/actions/navigate.tsx
+++ app/actions/navigate.tsx
@@ -1,7 +1,12 @@
-"use server"
-import { redirect } from "next/navigation"
+"use server";
+import { redirect } from "next/navigation";
 
-export default async function navigate(route,id){
-    redirect(`/${route}/${id}`)
+export default async function navigate({
+  route,
+  id,
+}: {
+  route: string;
+  id: string;
+}) {
+  redirect(`/${route}/${id}`);
 }
-

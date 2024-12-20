0183334d6c0c1a88575193280066b2fd4aa72543
omer mohamed <damouramory@gmail.com>
2024-12-20 18:19:22 +0200

    remove: dynamic client pages

--- app/posts/[id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
-export default function loading(){
-    return(
-        <Skeleton className="h-96 bg-slate-600">
-
-        </Skeleton>
-    )
-}

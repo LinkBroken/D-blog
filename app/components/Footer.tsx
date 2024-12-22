9037417875de925de103898308fb80f9ea3f9bfa
omer mohamed <damouramory@gmail.com>
2024-12-22 20:13:42 +0200

    chore footer styles

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,7 +10,7 @@ interface footerProps {
 export default function Footer({ description, icon, title }: footerProps) {
   return (
     <>
-      <div className="flex justify-center text-white gap-4 p-8 bg-teal-700 items-center mt-10 bottom-0 w-full bg-inherit">
+      <div className="flex justify-center mt-96 text-white gap-4 p-8 bg-teal-700 items-center  bottom-0 w-full bg-inherit ">
         {title && <h2 className="text-2xl font-bold">{title}</h2>}
         {icon && <FontAwesomeIcon className="w-44" icon={icon ?? undefined} />}
         {description && <p className="text-sm mt-2">{description}</p>}
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
   const [menuClick, setMenuClick] = useState<boolean | null>(false);
 
   return (
-    <nav className="flex fixed top-0 left-0 z-40 w-32 h-screen transition-transform  sm:translate-x-0 flex-col justify-between text-white bg-teal-700 p-0 md:p-4 lg:justify-evenly">
+    <nav className="flex fixed top-0 left-0 z-40 w-32 h-screen transition-transform  sm:translate-x-0 flex-col justify-between text-white bg-teal-700 p-0 md:p-6 lg:justify-evenly">
       <FontAwesomeIcon
         onClick={() => setMenuClick(!menuClick)}
         className="w-32 md:hidden"
@@ -24,20 +24,23 @@ export default function Navbar() {
         className={`${
           menuClick
             ? "grid absolute grid-cols-1 place-items-start pl-6 items-center justify-between bg-inherit h-1/2 w-2/3 top-12 left-0 text-2xl duration:300 transition-all duration-2000 ease-in-out md:flex md:static md:justify-between md:flex-col md:p-0 "
-            : "hidden relative md:gap-4  md:p-0 md:flex md:flex-col md:justify-between md:w-2/3 md:text-xl"
+            : "hidden relative md:gap-4 md:items-center  md:p-0 md:flex md:flex-col md:justify-between md:w-2/3 md:text-xl"
         }`}
       >
-        <Link className="hover:bg-teal-800 p-2 rounded-xl lg:inline" href="/">
+        <Link
+          className="hover:bg-teal-800 p-4 rounded-xl w-fit lg:inline"
+          href="/"
+        >
           Home
         </Link>
         <Link
-          className="hover:bg-teal-800 p-2 rounded-xl lg:inline "
+          className="hover:bg-teal-800 p-4 rounded-xl w-fit lg:inline"
           href="/posts"
         >
           Posts
         </Link>
         <Link
-          className=" hover:bg-teal-800 p-2 rounded-xl lg:inline "
+          className="hover:bg-teal-800 p-4 rounded-xl w-fit lg:inline"
           href="/users"
         >
           Authors

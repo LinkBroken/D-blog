7fd8d4f29bbc15cf9f2aa16cbd8b361509b15560
omer mohamed <damouramory@gmail.com>
2024-12-22 19:20:50 +0200

    refactor: navbar look

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,25 +7,26 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function Navbar() {
   const [menuClick, setMenuClick] = useState<boolean | null>(false);
 
   return (
-    <nav className="flex justify-between text-white bg-teal-700 p-4 lg:justify-evenly">
+    <nav className="flex fixed top-0 left-0 z-40 w-32 h-screen transition-transform  sm:translate-x-0 flex-col justify-between text-white bg-teal-700 p-0 md:p-4 lg:justify-evenly">
       <FontAwesomeIcon
         onClick={() => setMenuClick(!menuClick)}
-        className="w-8 md:hidden"
+        className="w-32 md:hidden"
         icon={burgerMenu}
+        size="3x"
       />
       <div
         className={`${
           menuClick
-            ? "grid absolute grid-cols-1 place-items-start pl-6 items-center justify-between bg-inherit h-1/2 w-2/3 top-12 left-0 text-2xl duration:300 transition-all duration-2000 ease-in-out md:flex md:static md:justify-between md:flex-row md:p-0 "
-            : "hidden relative md:flex-row md:p-0 md:flex md:justify-between md:w-2/3 md:text-xl"
+            ? "grid absolute grid-cols-1 place-items-start pl-6 items-center justify-between bg-inherit h-1/2 w-2/3 top-12 left-0 text-2xl duration:300 transition-all duration-2000 ease-in-out md:flex md:static md:justify-between md:flex-col md:p-0 "
+            : "hidden relative md:gap-4  md:p-0 md:flex md:flex-col md:justify-between md:w-2/3 md:text-xl"
         }`}
       >
-        <h1 className="self-center text-2xl text-white md:inline ">D-blog</h1>
         <Link className="hover:bg-teal-800 p-2 rounded-xl lg:inline" href="/">
           Home
         </Link>
@@ -39,20 +40,15 @@ export default function Navbar() {
           className=" hover:bg-teal-800 p-2 rounded-xl lg:inline "
           href="/users"
         >
-          Users
-        </Link>
-
-        <Link
-          className="hidden bg-teal-700 hover:bg-teal-800  p-2 rounded-xl lg:inline "
-          href="/login"
-        >
-          Login
+          Authors
         </Link>
       </div>
-      <Link className="flex items-center gap-4" href="/account">
-        <span className="hidden md:inline"> Account</span>
-        <FontAwesomeIcon icon={icon} size="lg" className="w-8 md:hidden" />
-      </Link>
+      <Button
+        className=" mb-10 shadow-md shadow-black bg-inherit text-white hover:text-black"
+        variant={"secondary"}
+      >
+        <Link href="/add">Add a post</Link>
+      </Button>
     </nav>
   );
 }
